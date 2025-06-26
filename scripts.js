8056717f6db0475eecc9da0eda57730333d84bf9
arian <arian.maki@yahoo.com>
2025-06-26 02:37:25 +0200

    loading products from an external file

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
+import { productsData } from "./products.js";
+
 const cartBtn = document.querySelector(".navbar__cart");
 const cartModal = document.querySelector(".cart");
 const backDrop = document.querySelector(".backdrop");
 const confirmBtn = document.querySelector(".cart-item-confirm");
+const productsDOM = document.querySelector(".products__grid");
 
 function showModal() {
   cartModal.style.opacity = "1";
@@ -18,3 +21,43 @@ function closeModal() {
 cartBtn.addEventListener("click", showModal);
 confirmBtn.addEventListener("click", closeModal);
 backDrop.addEventListener("click", closeModal);
+
+// 1. get products
+class Products {
+  getProducts() {
+    return productsData;
+  }
+}
+
+// 2. display products
+class UI {
+  displayProducts(products) {
+    let result = "";
+    products.forEach((item) => {
+      result += `<div class="products__item">
+              <div class="product__img-container">
+                <img src=${item.imageUrl} alt="" />
+              </div>
+              <div class="products__details">
+                <p class="products__name">${item.title}</p>
+                <p class="products__price">${item.price}$</p>
+              </div>
+              <button class="btn add-to-cart data-id=${item.id}">
+                <i class="fas fa-shopping-cart"></i>
+                add to cart
+              </button>
+            </div>`;
+    });
+    productsDOM.innerHTML = result;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const products = new Products();
+  const productsData = products.getProducts();
+  const ui = new UI();
+  ui.displayProducts(productsData);
+});
+
+//3. storage
+class Storage {}
